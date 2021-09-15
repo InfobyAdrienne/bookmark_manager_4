df8ee67d005170f2594d59dbca57fac95204ca87
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-09-15 11:21:23 +0100

    Added Bookmark class, feature and unit tests

--- spec/feature/viewing_bookmarks_spec.rb
+++ spec/feature/viewing_bookmarks_spec.rb
@@ -1,6 +1,16 @@
 feature 'Viewing bookmarks' do
   scenario 'visiting the index page' do
     visit('/')
-    expect(page).to have_content 'Bookmark Manager'
+    expect(page).to have_content "Bookmark Manager"
+  end
+end
+
+feature 'Viewing bookmarks' do
+  scenario 'User can see all bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.youtube.com"
+    expect(page).to have_content "http://www.twitter.com"
   end
 end
